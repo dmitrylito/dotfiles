fc2f35e1ab47893e7cbf8d36afaafd31cee4c690
dmitrylito <dmitrylitoshik15@gmail.com>
2025-11-19 02:26:56 -0500

    Dot files updated

--- home/.config/nvim/lua/plugins/fzf-lua.lua
+++ home/.config/nvim/lua/plugins/fzf-lua.lua
@@ -1,15 +1,19 @@
 return {
-  {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- or if using mini.icons/mini.nvim
-    -- dependencies = { "nvim-mini/mini.icons" },
-    opts = {
-      files = {
-        hidden = true,
-        follow = true,
-      },
-    },
-  },
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- or if using mini.icons/mini.nvim
+		-- dependencies = { "nvim-mini/mini.icons" },
+		opts = {
+			files = {
+				hidden = true,
+				follow = true,
+			},
+			grep = {
+				hidden = true,
+				follow = true,
+			},
+		},
+	},
 }
