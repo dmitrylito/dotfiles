5e1b6e009960036f9664ece420c9b039e1bf5112
Dmitry Litoshik <dmitrylitoshik15@gmail.com>
2025-11-22 00:14:53 -0500

    Dot files updated

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,48 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			explorer = {},
+
+			picker = {
+				layouts = {
+					default = {
+						reverse = true,
+						layout = {
+							box = "horizontal",
+							width = 0.95,
+							height = 0.95,
+
+							{
+								box = "vertical",
+								border = "rounded",
+								title = "{title} {live} {flags}",
+
+								{ win = "list", border = "none" },
+								{ win = "input", height = 1, border = "top" },
+							},
+
+							{
+								win = "preview",
+								title = "{preview}",
+								border = "rounded",
+								width = 0.65,
+							},
+						},
+					},
+				},
+				sources = {
+					files = {
+						follow = true,
+						hidden = true,
+						ignored = true,
+					},
+					explorer = {
+						hidden = true,
+						follow = true,
+					},
+				},
+			},
+		},
+	},
+}
