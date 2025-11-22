3a423328266ca60a666b0555df0ef3a583308ea7
dmitrylito <dmitrylitoshik15@gmail.com>
2025-11-22 16:25:39 -0500

    Dot files updated

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -52,45 +52,45 @@ return {
           { section = "startup" },
         },
       },
-    },
-    explorer = {},
+      explorer = {},
 
-    picker = {
-      layouts = {
-        default = {
-          reverse = true,
-          layout = {
-            box = "horizontal",
-            width = 0.95,
-            height = 0.95,
+      picker = {
+        layouts = {
+          default = {
+            reverse = true,
+            layout = {
+              box = "horizontal",
+              width = 0.95,
+              height = 0.95,
 
-            {
-              box = "vertical",
-              border = "rounded",
-              title = "{title} {live} {flags}",
+              {
+                box = "vertical",
+                border = "rounded",
+                title = "{title} {live} {flags}",
 
-              { win = "list", border = "none" },
-              { win = "input", height = 1, border = "top" },
-            },
+                { win = "list", border = "none" },
+                { win = "input", height = 1, border = "top" },
+              },
 
-            {
-              win = "preview",
-              title = "{preview}",
-              border = "rounded",
-              width = 0.65,
+              {
+                win = "preview",
+                title = "{preview}",
+                border = "rounded",
+                width = 0.65,
+              },
             },
           },
         },
-      },
-      sources = {
-        files = {
-          follow = true,
-          hidden = true,
-          ignored = true,
-        },
-        explorer = {
-          hidden = true,
-          follow = true,
+        sources = {
+          files = {
+            follow = true,
+            hidden = true,
+            ignored = true,
+          },
+          explorer = {
+            hidden = true,
+            follow = true,
+          },
         },
       },
     },
