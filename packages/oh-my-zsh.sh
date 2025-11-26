b1bf539d297cb7a102e10cd34d0ad08adc906394
Dmitry Litoshik <dmitrylitoshik15@gmail.com>
2025-11-26 16:33:45 -0500

    Dot files updated

--- packages/oh-my-zsh.sh
+++ packages/oh-my-zsh.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
 
+REAL_HOME=""
+EXEC_USER_CMD=""
+if [ -n "${SUDO_USER}" ]; then
+  REAL_HOME=$(getent passwd "${SUDO_USER}" | cut -d: -f6)
+  EXEC_USER_CMD="sudo -u ${SUDO_USER}"
+else
+  REAL_HOME="${HOME}"
+fi
+
 echo "Installing Oh My Zsh..."
 
-if [ -d "$HOME/.oh-my-zsh" ]; then
+if [ -d "${REAL_HOME}/.oh-my-zsh" ]; then
     echo "Oh My Zsh is already installed. Skipping installation."
 else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    ${EXEC_USER_CMD} sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 echo "Oh My Zsh installation script finished."
