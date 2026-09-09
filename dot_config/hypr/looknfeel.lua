2078ede7974d4ebf6ad10de559423febf4bfffe4
Dmitry Litoshik <dmitrylitoshik@fleetchaser.com>
2026-09-09 08:24:42 -0400

    Update .config/hypr/looknfeel.lua Update .config/voxtype/config.toml

--- dot_config/hypr/looknfeel.lua
+++ dot_config/hypr/looknfeel.lua
@@ -1,30 +1,30 @@
 hl.config({
-  general = {
-    gaps_in = 5,
-    gaps_out = 12,
-    border_size = 4,
-    layout = "dwindle",
-    allow_tearing = true,
-    snap = {
-      enabled = true,
-      respect_gaps = true,
-    },
-  },
-  decoration = {
-    rounding = 10,
-    active_opacity = 1.0,
-    inactive_opacity = 1.0,
-  },
-  scrolling = {
-    fullscreen_on_one_column = true,
-    column_width = 0.5,
-    follow_focus = true,
-    focus_fit_method = 1,
-    follow_min_visible = 0,
-  },
-  misc = {
-    mouse_move_enables_dpms = false,
-  },
+	general = {
+		gaps_in = 4,
+		gaps_out = 8,
+		border_size = 3,
+		layout = "dwindle",
+		allow_tearing = true,
+		snap = {
+			enabled = true,
+			respect_gaps = true,
+		},
+	},
+	decoration = {
+		rounding = 10,
+		active_opacity = 1.0,
+		inactive_opacity = 1.0,
+	},
+	scrolling = {
+		fullscreen_on_one_column = true,
+		column_width = 0.5,
+		follow_focus = true,
+		focus_fit_method = 1,
+		follow_min_visible = 0,
+	},
+	misc = {
+		mouse_move_enables_dpms = false,
+	},
 })
 
 -- Omarchy's default-opacity windowrule (0.985 0.96) overrides decoration
