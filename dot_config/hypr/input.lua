27f6db949367694c28f04fb0fc291e37f0fad92e
Dmitry Litoshik <dmitrylitoshik@fleetchaser.com>
2026-09-21 12:24:55 -0400

    Update .config/hypr/input.lua

--- dot_config/hypr/input.lua
+++ dot_config/hypr/input.lua
@@ -8,7 +8,7 @@ hl.config({
 		accel_profile = "adaptive",
 		follow_mouse = 1,
 		sensitivity = 0.4,
-		scroll_factor = 1.5,
+		scroll_factor = 1.2,
 		natural_scroll = false,
 		touchpad = {
 			scroll_factor = 0.4,
